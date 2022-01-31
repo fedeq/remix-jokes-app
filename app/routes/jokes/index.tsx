5e9691eaefbb31d2b01f20991c330146e29dbd9d
Federico Quattrocchio <federicoquattrocchio@gmail.com>
2022-01-30 23:49:00 -0300

    Added prisma and data fetch

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,27 @@
+import { Joke } from "@prisma/client";
+import { LoaderFunction, useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  const data: LoaderData = { joke: randomJoke };
+  return data;
+};
+
+export default function JokesIndexRoute() {
+  const { joke } = useLoaderData<LoaderData>();
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{joke.content}</p>
+    </div>
+  );
+}
